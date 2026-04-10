0686ce522fc2eab0bb8190ba88de10294a1538c5
Vivek Shenoy <vivekshenoy6763@gmail.com>
2026-04-10 23:51:10 +0530

    implemented the router of /auth/me

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { signup, login, logout } from "../controllers/authController.js";
-
+import protect from "../middleware/authMiddleware.js"
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+
+router.get("/me", protect, (req, res) => {
+  res.json({ user: req.user });
+});
 
 export default router;
