b692dd86097b6bc7c74628d218a0c5654be79034
Vivek Shenoy <vivekshenoy6763@gmail.com>
2026-04-11 11:04:03 +0530

    mongodb setup for render

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,14 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is missing");
+    }
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
   } catch (err) {
-    console.log(err);
+    console.log("DB Error:", err.message);
     process.exit(1);
   }
 };
